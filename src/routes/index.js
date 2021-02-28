4b3a77e31a685f049be4223663fde0d05d601509
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-02-27 22:15:59 -0300

    feat: add status on root

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,15 @@ const routes = Router();
 routes.use("/posts", postsRouter);
 routes.use("/feeds", feedRouter);
 
+routes.get("/", (req, res) => {
+  status: "up";
+  endpoints: [
+    {
+      retrievePosts: "GET /posts?page=1",
+      addFeed: "POST /feeds",
+      retrieveFeeds: "GET /feeds",
+    },
+  ];
+});
+
 export default routes;
