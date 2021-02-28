eacc98f016669bb7ca0c95801e3645818313d24e
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-02-27 22:25:02 -0300

    feat: adding postType

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,17 @@ const routes = Router();
 routes.use("/posts", postsRouter);
 routes.use("/feeds", feedRouter);
 
-routes.get("/", (req, res) => {
-  status: "up";
-  endpoints: [
-    {
-      retrievePosts: "GET /posts?page=1",
-      addFeed: "POST /feeds",
-      retrieveFeeds: "GET /feeds",
-    },
-  ];
-});
+routes.get("/", (req, res) =>
+  res.json({
+    status: "up",
+    endpoints: [
+      {
+        retrievePosts: "GET /posts?page=1",
+        addFeed: "POST /feeds",
+        retrieveFeeds: "GET /feeds",
+      },
+    ],
+  })
+);
 
 export default routes;
